cd653b2bb238fdc87d6424c3a8f28f25a159e799
kienvipmkvn <kienvipmkvn3199@gmail.com>
2023-12-10 21:35:01 +0700

    done deploy

--- DATN-GracefulStyleShop-ADMIN/src/router/index.js
+++ DATN-GracefulStyleShop-ADMIN/src/router/index.js
@@ -3,8 +3,9 @@ import {createRouter,createWebHistory } from 'vue-router'
 import routes from '@/router/routes'
 
 const router = createRouter({
-    history : createWebHistory(),
-    routes
+    history : createWebHistory(process.env.VUE_APP_PUBLIC_PATH),
+    routes,
+    
 })
 
 export default router;
